763bc724d99e913dff465c84e0585b41a4ed2a35
csjpeter <csjpeter@gmail.com>
2022-11-17 09:28:45 +0100

    Bit nicer TBW output

--- TBW.sh
+++ TBW.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "/dev/sda"
+echo "--------"
 if sudo smartctl -a /dev/sda | grep "^Device Model:" | grep "Samsung SSD 850 EVO 1TB"; then
 	sudo smartctl -A /dev/sda | grep "^241"
 	sudo smartctl -A /dev/sda | awk '/^241/ { print "/dev/sda TBW: "($10 * 512) * 1.0e-12, "TB" }'
@@ -7,10 +9,27 @@ elif sudo smartctl -a /dev/sda | grep "^Device Model:" | grep "KINGSTON SKC400S3
 	sudo smartctl -A /dev/sda | awk '/^241/ { print "/dev/sda TBW: "$10 * 1.0e-3, "TB" }'
 fi
 
-if sudo smartctl -a /dev/sdb | grep "^Device Model:" | grep "KINGSTON SA400S37480G"; then
+echo
+echo "/dev/sdb"
+echo "--------"
+
+if sudo smartctl -a /dev/sdb | grep "^Device Model:" | grep "HITACHI HTS543232L9SA00"; then
 	sudo smartctl -A /dev/sdb | grep "^241"
 	sudo smartctl -A /dev/sdb | awk '/^241/ { print "/dev/sdb TBW: "$10 * 1.0e-3, "TB" }'
 fi
+
+echo
+echo "/dev/sdc"
+echo "--------"
+
+if sudo smartctl -a /dev/sdc | grep "^Device Model:" | grep "KINGSTON SA400S37480G"; then
+	sudo smartctl -A /dev/sdc | grep "^241"
+	sudo smartctl -A /dev/sdc | awk '/^241/ { print "/dev/sdc TBW: "$10 * 1.0e-3, "TB" }'
+fi
+
+echo
+echo "/dev/nvme0n1"
+echo "------------"
 
 if sudo smartctl -a /dev/nvme0n1 | grep "^Model Number:" | grep "Samsung SSD 970 EVO Plus"; then
 	#sudo smartctl -a /dev/nvme0n1 | grep "^Model Number:"
